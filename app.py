3f58f25d722bb283931e83db726da10bda2bc8fd
Asma Afshar <161859558+AsmaAfshar@users.noreply.github.com>
2025-03-08 23:01:41 +0500

    Add files via upload

--- app.py
+++ app.py
@@ -1,7 +1,11 @@
 #streamlit
 import streamlit as st
 
-st.set_page_config(page_title= "growth inside project", project_icon= "★")
+import streamlit as st
+
+import streamlit as st
+
+st.set_page_config(page_title= "growth mindside project", project_icon="✬")
 st.title("Growth Mindset Challenge: Web App with Streamlit")
 
 st.header("🚀 Welcome to Your Growth Journey!")
